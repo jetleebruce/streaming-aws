21e53e4dd164533a3068c5e66400748a0622c5e9
Serge Modin <modinsv@yandex.ru>
2020-07-25 09:22:30 +0300

    createComment and  listComments now fetching from the server with help of reducer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import ReactPlayer from "react-player";
 import { HashRouter, Link, Switch, Route } from "react-router-dom";
 import AuthComponent from "./AuthComponent";
-import { Auth } from "aws-amplify";
+import { Auth, API } from "aws-amplify";
+import { createComment as CreateComment } from "./graphql/mutations";
+import { listComments as ListComments } from "./graphql/queries";
+import { onCreateComment as OnCreateComment } from "./graphql/subscriptions";
 
 const streamUrl =
   "https://8a2e6418a928.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.675692464543.channel.1JDFWcjTNrpy.m3u8";
@@ -27,14 +30,65 @@ function Router() {
   );
 }
 
-function App() {
-  const [user, setUser] = React.useState(null);
-  React.useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then((currentUser) => setUser(currentUser))
-      .catch((err) => console.log({ err }));
-  }, []);
+const initialState = {
+  comments: [],
+};
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_COMMENTS":
+      return { ...state, comments: action.comments };
+
+    case "ADD_COMMENT":
+      return { ...state, comments: [...state.comments, action.comment] };
+
+    default:
+      return state;
+  }
+}
+
+function App() {
+    const [user, setUser] = React.useState(null);
+    const [inputValue, setInput] = React.useState("");
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    React.useEffect(() => {
+      Auth.currentAuthenticatedUser()
+        .then((currentUser) => setUser(currentUser))
+        .catch((err) => console.log({ err }));
+      fetchComments();
+      
+    }, []);
+
+   async function fetchComments() {
+     const commentData = await API.graphql({
+       query: ListComments,
+     });
+     dispatch({
+       type: "SET_COMMENTS",
+       comments: commentData.data.listComments.items,
+     });
+   } 
+
+  async function createComment() {
+    if (!inputValue) return;
+    const message = inputValue;
+    setInput("");
+    dispatch({
+      type: "ADD_COMMENT",
+      comment: { message, owner: user.username },
+    });
+    await API.graphql({
+      query: CreateComment,
+      variables: {
+        input: { message },
+      },
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    });
+  }
+  function onChange(e) {
+    e.persist();
+    setInput(e.target.value);
+  }
   return (
     <div className='App'>
       <div style={{ display: "flex" }}>
@@ -42,14 +96,22 @@ function App() {
           <ReactPlayer url={streamUrl} width='100%' height='100%' playing />
         </div>
         <div style={{ width: 300, border: "1px solid grey" }}>
-          {
-            user && (
-              <div>
-                <input placeholder="comment"/>
-                <button>Create a comment</button>
-              </div>
-            )
-          }
+          {user && (
+            <div>
+              <input
+                value={inputValue}
+                onChange={onChange}
+                placeholder='comment'
+              />
+              <button onClick={createComment}>Create a comment</button>
+            </div>
+          )}
+          {state.comments.map((comment, index) => (
+            <div key={index}>
+              <p>{comment.message}</p>
+              <span>From: {comment.owner}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
