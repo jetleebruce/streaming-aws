9990e740d830e28f5ce70cd4b06382a3c5db2a9f
Serge Modin <modinsv@yandex.ru>
2020-07-25 10:02:18 +0300

    Get messages in real time from DB

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import AuthComponent from "./AuthComponent";
 import { Auth, API } from "aws-amplify";
 import { createComment as CreateComment } from "./graphql/mutations";
 import { listComments as ListComments } from "./graphql/queries";
-import { onCreateComment as OnCreateComment } from "./graphql/subscriptions";
+import {
+  onCreateComment as OnCreateComment,
+  onCreateComment,
+} from "./graphql/subscriptions";
 
 const streamUrl =
   "https://8a2e6418a928.eu-west-1.playback.live-video.net/api/video/v1/eu-west-1.675692464543.channel.1JDFWcjTNrpy.m3u8";
@@ -48,26 +51,45 @@ function reducer(state, action) {
 }
 
 function App() {
-    const [user, setUser] = React.useState(null);
-    const [inputValue, setInput] = React.useState("");
-    const [state, dispatch] = React.useReducer(reducer, initialState);
-    React.useEffect(() => {
-      Auth.currentAuthenticatedUser()
-        .then((currentUser) => setUser(currentUser))
-        .catch((err) => console.log({ err }));
-      fetchComments();
-      
-    }, []);
+  const [user, setUser] = React.useState(null);
+  const [inputValue, setInput] = React.useState("");
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  React.useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((currentUser) => setUser(currentUser))
+      .catch((err) => console.log({ err }));
+    fetchComments();
+    subscribe();
+  }, []);
 
-   async function fetchComments() {
-     const commentData = await API.graphql({
-       query: ListComments,
-     });
-     dispatch({
-       type: "SET_COMMENTS",
-       comments: commentData.data.listComments.items,
-     });
-   } 
+  async function subscribe() {
+    API.graphql({
+      query: onCreateComment,
+    }).subscribe({
+      next: async (commentData) => {
+        console.log(commentData);
+        const {value:{data}}=commentData
+        try {
+          const user = await Auth.currentAuthenticatedUser()
+          if (user.username === data.onCreateComment.owner) {
+            return
+          }
+          dispatch({type: "ADD_COMMENT", comment: data.onCreateComment})
+        } catch (err) {
+          dispatch({ type: "ADD_COMMENT", comment: data.onCreateComment });
+        }
+      },
+    });
+  }
+  async function fetchComments() {
+    const commentData = await API.graphql({
+      query: ListComments,
+    });
+    dispatch({
+      type: "SET_COMMENTS",
+      comments: commentData.data.listComments.items,
+    });
+  }
 
   async function createComment() {
     if (!inputValue) return;
